251d5af6ce88a9b3a9a5acebc590e0bde3d6c28d
pcauchy1 <144147963+pcauchy1@users.noreply.github.com>
2024-09-25 11:53:22 -0400

    Added testing data

--- ARPES_Calendar.py
+++ ARPES_Calendar.py
@@ -3,6 +3,7 @@ import os.path
 import htmdec_formats
 import os
 import time
+import pytz
 
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
@@ -11,10 +12,10 @@ from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
 # If modifying these scopes, delete the file token.json.
-SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"]
+SCOPES = ["https://www.googleapis.com/auth/calendar"]
 
 
-def main(wavenote_file):
+def main(wavenote_file = None):
     """Shows basic usage of the Google Calendar API.
     Prints the start and name of the next 10 events on the user's calendar.
     """
@@ -38,61 +39,68 @@ def main(wavenote_file):
         with open("token.json", "w") as token:
             token.write(creds.to_json())
 
-    file_folder = os.listdir(os.path.dirname(wavenote_file)) 
-    for name in file_folder:
-        if not '.pxt' in name:
-            file_folder.remove(name)
-    sorted_files = sorted(file_folder, key=lambda x: int(x.split('_')[1].split('.')[0]))
-    length = len(sorted_files)
-    for file in sorted_files:
-        if count == 0:
-            data_file = os.path.join(os.path.dirname(wavenote_file), "data_holder.txt")
-            dataset = htmdec_formats.ARPESDataset.from_file(wavenote_file)
-            with open(data_file, "w") as f:
-                l = f.write(dataset._metadata)
-            with open(data_file, 'r') as file:
-                lines = file.readlines()
-            os.remove(data_file)
-            start = [lines[28].split('=')[1],lines[29].split('=')[1]]
-        elif count == length - 1:
-            ti_m = os.path.getmtime(wavenote_file)
-            m_ti = time.ctime(ti_m) 
-            if ':' in m_ti.split(' ')[3]:
-                end_time = m_ti.split(' ')[3]
-            else:
-                end_time = m_ti.split(' ')[4]
-            if ':' in m_ti.split(' ')[3]:
-                end_date = m_ti.split(' ')[0] + '-' + m_ti.split(' ')[1] + '-' + m_ti.split(' ')[2]
-            else:
-                end_date = m_ti.split(' ')[0] + '-' + m_ti.split(' ')[1] + '-' + m_ti.split(' ')[3]
-            end = [end_date, end_time]
+    if wavenote_file:
+        file_folder = os.listdir(os.path.dirname(wavenote_file)) 
+        for name in file_folder:
+            if not '.pxt' in name:
+                file_folder.remove(name)
+        sorted_files = sorted(file_folder, key=lambda x: int(x.split('_')[1].split('.')[0]))
+        length = len(sorted_files)
+        for file in sorted_files:
+            if count == 0:
+                data_file = os.path.join(os.path.dirname(wavenote_file), "data_holder.txt")
+                dataset = htmdec_formats.ARPESDataset.from_file(wavenote_file)
+                with open(data_file, "w") as f:
+                    l = f.write(dataset._metadata)
+                with open(data_file, 'r') as file:
+                    lines = file.readlines()
+                os.remove(data_file)
+                start = [lines[28].split('=')[1],lines[29].split('=')[1]]
+            elif count == length - 1:
+                ti_m = os.path.getmtime(wavenote_file)
+                m_ti = time.ctime(ti_m) 
+                if ':' in m_ti.split(' ')[3]:
+                    end_time = m_ti.split(' ')[3]
+                else:
+                    end_time = m_ti.split(' ')[4]
+                if ':' in m_ti.split(' ')[3]:
+                    end_date = m_ti.split(' ')[0] + '-' + m_ti.split(' ')[1] + '-' + m_ti.split(' ')[2]
+                else:
+                    end_date = m_ti.split(' ')[0] + '-' + m_ti.split(' ')[1] + '-' + m_ti.split(' ')[3]
+                end = [end_date, end_time]
+
+    timezone = pytz.timezone('America/New_York')
+    # Current time in the given timezone
+    current_time = datetime.datetime.now(timezone)
+    # Check if daylight saving time is in effect
+    is_dst = bool(current_time.dst())  # .dst() returns a timedelta, so we convert to boolean
+    if is_dst:
+        offset = '05:00'
+    else:
+        offset = '04:00'
+    
+    final_start = start[0] + 'T' + start[1] + '-' + offset
+    final_end = end[0] + 'T' + end[1] + '-' + offset
+
     try:
         service = build("calendar", "v3", credentials=creds)
 
-        # Call the Calendar API
-        now = datetime.datetime.utcnow().isoformat() + "Z"  # 'Z' indicates UTC time
-        print("Getting the upcoming 10 events")
-        events_result = (
-            service.events()
-            .list(
-                calendarId="primary",
-                timeMin=now,
-                maxResults=10,
-                singleEvents=True,
-                orderBy="startTime",
-            )
-            .execute()
-        )
-        events = events_result.get("items", [])
+        event = {
+        'summary': 'ARPES User',
+        'location': '343 Campus Road, Ithaca, NY 14853',
+        'description': 'PARADIM user used ARPES during this time.',
+        'start': {
+            'dateTime': final_start,
+            'timeZone': 'America/New_York',
+        },
+        'end': {
+            'dateTime': final_end,
+            'timeZone': 'America/New_York',
+        }
+        }
 
-        if not events:
-            print("No upcoming events found.")
-            return
-
-        # Prints the start and name of the next 10 events
-        for event in events:
-            start = event["start"].get("dateTime", event["start"].get("date"))
-        print(start, event["summary"])
+        event = service.events().insert(calendarId='primary', body=event).execute()
+        print('Event created: %s' % (event.get('htmlLink')))
 
     except HttpError as error:
         print(f"An error occurred: {error}")
